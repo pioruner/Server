a293708fff4a95b15e52b9cf05354e2660603707
CIM <cim@kortekh.ru>
2024-10-25 22:45:51 +0300

    Init

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/goburrow/modbus"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	_ "log"
 	"os"
 	"time"
 )
@@ -12,23 +11,34 @@ import (
 func setupLogger() *zap.Logger {
 	config := zap.NewProductionEncoderConfig()
 	fileEncoder := zapcore.NewJSONEncoder(config)
-	logFile, _ := os.Create("app.log") // Открыть файл для записи логов
-	fileWriter := zapcore.AddSync(logFile)
+	consoleEncoder := zapcore.NewConsoleEncoder(config)
 
-	core := zapcore.NewCore(fileEncoder, fileWriter, zap.InfoLevel)
+	logFile, _ := os.Create("app.log")
+	fileWriter := zapcore.AddSync(logFile)
+	consoleWriter := zapcore.AddSync(os.Stdout)
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, fileWriter, zap.InfoLevel),
+		zapcore.NewCore(consoleEncoder, consoleWriter, zap.InfoLevel),
+	)
 	logger := zap.New(core)
 	return logger
 }
 
-func queryDevice(name string, address string, interval time.Duration, logger *zap.Logger) {
+func queryDevice(name, address string, slaveID byte, interval time.Duration, logger *zap.Logger) {
 	for {
 		handler := modbus.NewTCPClientHandler(address)
+		handler.SlaveId = slaveID // Установка Slave ID
+		logger.Info("Attempting to connect", zap.String("device", name), zap.String("address", address))
+
 		err := handler.Connect()
 		if err != nil {
 			logger.Warn("Failed to connect", zap.String("device", name), zap.Error(err))
-			time.Sleep(5 * time.Second) // Повторное подключение через 5 секунд
+			handler.Close() // Убедитесь, что соединение закрыто при неудаче
+			time.Sleep(5 * time.Second)
 			continue
 		}
+		logger.Info("Connected successfully", zap.String("device", name))
 
 		client := modbus.NewClient(handler)
 		defer handler.Close()
@@ -38,8 +48,8 @@ func queryDevice(name string, address string, interval time.Duration, logger *za
 			results, err := client.ReadHoldingRegisters(0, 10)
 			if err != nil {
 				logger.Error("Failed to read registers", zap.String("device", name), zap.Error(err))
-				handler.Close() // Закрываем соединение при ошибке
-				break           // Переходим к переподключению
+				handler.Close()
+				break
 			} else {
 				logger.Info("Read registers", zap.String("device", name), zap.ByteString("data", results))
 			}
@@ -51,8 +61,9 @@ func main() {
 	logger := setupLogger()
 	defer logger.Sync()
 
-	go queryDevice("Modbus1", "localhost:5020", 2*time.Second, logger)
-	go queryDevice("Modbus2", "localhost:5021", 5*time.Second, logger)
+	// Укажите адреса, порты и Slave ID для каждого устройства
+	go queryDevice("Modbus1", "192.168.1.64:502", 1, 2*time.Second, logger) // Slave ID 1
+	//go queryDevice("Modbus2", "192.168.1.65:502", 2, 5*time.Second, logger) // Slave ID 2
 
-	select {} // Ожидание вечно
+	select {}
 }
